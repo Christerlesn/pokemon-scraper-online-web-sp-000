2a9e62c82292122e11950e5be095195f405881bc
Christerle Nounoute <christerlesn@gmail.com>
2020-12-30 02:29:45 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,12 @@
 class Pokemon
-  attr_accessor :file, :parsed_file, :all_pokemon, :db
-
-  def initialize
-
+#  attr_accessor :file, :parsed_file, :all_pokemon, :db
+  attr_accessor :id, :name, :type, :db
+#to call on the scraper, just do Scraper_name.scraper_method
+  def initialize(id=nil, name, type, db)
+    @id = id 
+    @name = name
+    @type = type
+    @db = db
   end
   #The Pokemon class is responsible for saving, adding, removing, or changing anything about each Pokémon.
 end
