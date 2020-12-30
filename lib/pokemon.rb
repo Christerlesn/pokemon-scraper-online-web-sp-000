58adab68ffe9d4ef1cd1c8399c7d9844d01c6f13
Christerle Nounoute <christerlesn@gmail.com>
2020-12-30 02:31:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
 #  attr_accessor :file, :parsed_file, :all_pokemon, :db
   attr_accessor :id, :name, :type, :db
 #to call on the scraper, just do Scraper_name.scraper_method
-  def initialize(id=nil, name, type, db)
+  def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
     @type = type
