4906caf77916a744156cd3453c899b0c4d2a7a03
Christerle Nounoute <christerlesn@gmail.com>
2020-12-30 01:48:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,8 @@
 class Pokemon
+  attr_accessor :file, :parsed_file, :all_pokemon, :db
+
+  def initialize
+
+  end
+  #The Pokemon class is responsible for saving, adding, removing, or changing anything about each Pokémon.
 end
